382159228cd7a835250f689eed5e2fd3c549b3e9
Anthony Bobrov <anton.bobrov@hotmail.com>
2023-11-22 12:27:36 +0400

    HeaderExpandMenuButton: update icon

--- src/layout/Header/ExpandMenuButton/index.tsx
+++ src/layout/Header/ExpandMenuButton/index.tsx
@@ -30,16 +30,16 @@ export const HeaderExpandMenuButton: FC<IProps> = ({
       onClick={() => store.dispatch(menuSlice.actions.toggle())}
     >
       <svg
-        width="20"
-        height="20"
-        viewBox="0 0 20 20"
+        width="18"
+        height="18"
+        viewBox="0 0 18 18"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
           fillRule="evenodd"
           clipRule="evenodd"
-          d="M11 1H9V9L1 9V11H9V19H11V11H19V9L11 9V1Z"
+          d="M10 0H8V8L0 8V10H8V18H10V10H18V8L10 8V0Z"
           fill="currentColor"
         />
       </svg>
