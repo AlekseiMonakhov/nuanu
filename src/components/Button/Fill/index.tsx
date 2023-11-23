aef57dd7d65e2f8aed831b25b0b0b49e233b335b
Anthony Bobrov <anton.bobrov@hotmail.com>
2023-11-23 22:24:36 +0400

    FillButton: add `size` & `hasArrow`

--- src/components/Button/Fill/index.tsx
+++ src/components/Button/Fill/index.tsx
@@ -1,43 +1,89 @@
 import cn from 'classnames';
-import { forwardRef, memo } from 'react';
+import { forwardRef, memo, useEffect, useState } from 'react';
 import { ButtonAnchor } from '@anton.bobrov/react-components';
+import { useDebouncedProp } from '@anton.bobrov/react-hooks';
 import styles from './styles.module.scss';
 import { TFillButtonProps } from './types';
+import { DesktopArrow } from './icons/DesktopArrow';
+import { PhoneArrow } from './icons/PhoneArrow';
 
 const Component = forwardRef<
   HTMLButtonElement | HTMLAnchorElement,
   TFillButtonProps
->(({ className, style, text, theme, isSuccess, ...props }, ref) => (
-  <ButtonAnchor
-    ref={ref}
-    className={cn(
-      styles.button,
+>(
+  (
+    {
       className,
-      styles[`theme_${theme}`],
-      isSuccess && styles.success,
-    )}
-    style={style}
-    title={text}
-    {...props}
-  >
-    <span className={cn(styles.text, isSuccess && styles.hide)}>{text}</span>
+      style,
+      text,
+      theme,
+      size = 'medium',
+      hasArrow,
+      isSuccess: isSuccessProp,
+      ...props
+    },
+    ref,
+  ) => {
+    const [isSuccessRendered, setIsSuccessRendered] = useState(false);
+    const isSuccess = useDebouncedProp(isSuccessProp, 100);
 
-    <svg
-      className={cn(styles.success_icon, isSuccess && styles.show)}
-      width="21"
-      height="20"
-      viewBox="0 0 21 20"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      aria-hidden
-    >
-      <path
-        d="M18.2328 5.31955C18.6086 5.72426 18.5852 6.35699 18.1805 6.7328L8.48815 15.7328C8.10447 16.0891 7.51093 16.0891 7.12725 15.7328L2.81955 11.7328C2.41484 11.357 2.39141 10.7243 2.76721 10.3196C3.14301 9.91484 3.77574 9.89141 4.18046 10.2672L7.8077 13.6354L16.8196 5.26721C17.2243 4.89141 17.857 4.91484 18.2328 5.31955Z"
-        fill="currentColor"
-      />
-    </svg>
-  </ButtonAnchor>
-));
+    useEffect(() => {
+      if (isSuccessProp) {
+        setIsSuccessRendered(true);
+      }
+    }, [isSuccessProp]);
+
+    return (
+      <ButtonAnchor
+        ref={ref}
+        className={cn(
+          styles.button,
+          className,
+          styles[`theme_${theme}`],
+          styles[`size_${size}`],
+          isSuccess && styles.success,
+        )}
+        style={style}
+        title={text}
+        {...props}
+      >
+        <span
+          className={cn(
+            styles.content,
+            hasArrow && styles.has_arrow,
+            isSuccess && styles.hide,
+          )}
+        >
+          {text}
+
+          {hasArrow && (
+            <>
+              <DesktopArrow className={styles.desktop_arrow} />
+              <PhoneArrow className={styles.phone_arrow} />
+            </>
+          )}
+        </span>
+
+        {isSuccessRendered && (
+          <svg
+            className={cn(styles.success_icon, isSuccess && styles.show)}
+            width="21"
+            height="20"
+            viewBox="0 0 21 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden
+          >
+            <path
+              d="M18.2328 5.31955C18.6086 5.72426 18.5852 6.35699 18.1805 6.7328L8.48815 15.7328C8.10447 16.0891 7.51093 16.0891 7.12725 15.7328L2.81955 11.7328C2.41484 11.357 2.39141 10.7243 2.76721 10.3196C3.14301 9.91484 3.77574 9.89141 4.18046 10.2672L7.8077 13.6354L16.8196 5.26721C17.2243 4.89141 17.857 4.91484 18.2328 5.31955Z"
+              fill="currentColor"
+            />
+          </svg>
+        )}
+      </ButtonAnchor>
+    );
+  },
+);
 
 Component.displayName = 'FillButton';
 
--- src/components/Button/Fill/types.ts
+++ src/components/Button/Fill/types.ts
@@ -3,5 +3,7 @@ import { TButtonAnchor } from '@anton.bobrov/react-components';
 export type TFillButtonProps = TButtonAnchor & {
   text: string;
   theme: 'light' | 'dark';
+  size?: 'medium' | 'large';
+  hasArrow?: boolean;
   isSuccess?: boolean;
 };
