dea9ff9a8d51be764a128a4e08368e8baac95110
Anthony Bobrov <anton.bobrov@hotmail.com>
2023-11-28 13:44:26 +0400

    Upgrade react-kit

--- frontend/src/templates/Home/components/SectionSlider/utils/usePageScrollLock.ts
+++ frontend/src/templates/Home/components/SectionSlider/utils/usePageScrollLock.ts
@@ -1,23 +1,11 @@
-import {
-  isBrowser,
-  useEvent,
-  useEventListener,
-} from '@anton.bobrov/react-hooks';
+import { isBrowser, useEvent, useScrollLock } from '@anton.bobrov/react-hooks';
 import { useEffect } from 'react';
 
 export function usePageScrollLock(
   isEnabledProp: boolean,
   lockScrollClassName: string,
 ) {
-  // TODO: replace hook with one from react-kit (new release)
-  useEventListener({
-    ref: isBrowser ? window : null,
-    target: 'scroll',
-    listener: () => {
-      window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
-    },
-    isDisabled: !isEnabledProp,
-  });
+  useScrollLock(isBrowser ? window : null, { isDisabled: !isEnabledProp });
 
   const toggleDocumentScrollLock = useEvent((isEnabled: boolean) => {
     document.documentElement.classList.toggle(lockScrollClassName, isEnabled);
