e909f20fb5deae925560fffdb3e0e4699c6998f1
Anthony Bobrov <anton.bobrov@hotmail.com>
2024-02-15 16:27:15 +0400

    FillButton: add `theme=white`

--- frontend/src/components/Button/Fill/types.ts
+++ frontend/src/components/Button/Fill/types.ts
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 
 export type TFillButtonProps = TButtonAnchor & {
   text: string;
-  theme: 'light' | 'dark' | 'blue' | 'light_outline';
+  theme: 'light' | 'dark' | 'blue' | 'light_outline' | 'white';
   size: 50 | 60 | 80;
   sup?: string | null;
   children?: ReactNode;
