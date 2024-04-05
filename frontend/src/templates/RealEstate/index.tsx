7eba34a4b981272ca1ac5d4e5a6debcdd7b0cc6e
Aleksei Monakhov <lexmonakhov@yandex.ru>
2024-04-05 11:51:56 +0530

    add realEstate page, template, interface, styles, types

---
+++ frontend/src/templates/RealEstate/index.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react';
+import { useTemplate } from '../_hooks/useTemplate';
+import { ComingSoonWrapper } from '../components/ComingSoonWrapper';
+import styles from './styles.module.scss';
+import { IRealEstate } from './types';
+
+const RealEstate: FC<IRealEstate> = () => {
+  useTemplate();
+
+  return (
+    <ComingSoonWrapper
+      className={styles.template}
+      text="The 'RealEstate' page is coming soon."
+      items={[
+        {
+          key: 0,
+          original: '/lorem/accommodation/0.jpg',
+          width: 716,
+          height: 450,
+        },
+        {
+          key: 1,
+          original: '/lorem/accommodation/1.jpg',
+          width: 716,
+          height: 450,
+        },
+        {
+          key: 2,
+          original: '/lorem/accommodation/2.jpg',
+          width: 473,
+          height: 450,
+        },
+        {
+          key: 3,
+          original: '/lorem/accommodation/3.jpg',
+          width: 716,
+          height: 450,
+        },
+      ]}
+    />
+  );
+};
+
+export default RealEstate;
