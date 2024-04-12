89335ecc00610e88b6b868e1a88df61df2619742
Aleksei Monakhov <lexmonakhov@yandex.ru>
2024-04-12 16:49:16 +0530

    add black overlay under video

--- frontend/src/templates/RealEstate/components/FullScreenVideo/index.tsx
+++ frontend/src/templates/RealEstate/components/FullScreenVideo/index.tsx
@@ -1,22 +1,21 @@
-// @ts-nocheck
 import { FC } from "react";
-import styles from './styles.module.scss'
-import backgroundVideo from '..//../assets/videos/background.mp4';
+import styles from './styles.module.scss';
+import backgroundVideo from '../../assets/videos/background.mp4';
 import { IFullScreenVideo } from "./types";
 
 const FullScreenVideo: FC<IFullScreenVideo> = () => {
-
   return (
     <div id="home" className={styles.fullscreenVideo}>
+      <div className={styles.overlay}></div>
       <h1 className={styles.homeTitle}>
         Time to invest in Nuanu
-        </h1>
-        <video src={backgroundVideo}
-          loop
-          autoPlay
-          muted
-          playsInline
-          className={styles.videoBg} />
+      </h1>
+      <video src={backgroundVideo}
+        loop
+        autoPlay
+        muted
+        playsInline
+        className={styles.videoBg} />
     </div>
   );
 };
