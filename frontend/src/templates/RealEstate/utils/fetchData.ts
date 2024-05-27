7c50f22f1c638b966022eb2eb927b11584aa9f79
Aleksei M <lexmonakhov@gmail.com>
2024-05-28 01:41:45 +0545

    add ssl connection handle

--- frontend/src/templates/RealEstate/utils/fetchData.ts
+++ frontend/src/templates/RealEstate/utils/fetchData.ts
@@ -1,5 +1,5 @@
 export async function fetchRealEstateData() {
-  const apiUrl = "http://139.180.218.122/";
+  const apiUrl = "https://139.180.218.122:443/";
   const res = await fetch(`${apiUrl}realestate`);
   const data = await res.json();
   return data;
